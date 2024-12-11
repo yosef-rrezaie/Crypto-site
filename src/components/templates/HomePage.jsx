b831bc517d49c93c9e878e1ab0ae45548bba06d5
yosef-rrezaie <yosefrrezaie@gmail.com>
2024-12-11 16:34:29 +0330

    The initial files of the crypto project

---
+++ src/components/templates/HomePage.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import TableCoin from "../modules/TableCoin";
+import { getCoinnList } from "../../services/CryptoApi";
+
+function HomePage() {
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch(getCoinnList())
+      .then((res) => res.json())
+      .then((json) => setCoins(json));
+  }, []);
+  return (
+    <div>
+      <TableCoin coins={coins} />
+    </div>
+  );
+}
+
+export default HomePage;
