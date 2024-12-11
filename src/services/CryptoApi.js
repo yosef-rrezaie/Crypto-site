797fa6da42ed8acd8fca6a07d3f4473ca9567328
yosef-rrezaie <yosefrrezaie@gmail.com>
2024-12-11 17:10:30 +0330

    Getting coins information from api and showing it in the table

--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -2,7 +2,7 @@ const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-856jH6du7ANdiromKqwiM9Yx"
 
 const getCoinnList = () => {
-  return `${BASE_URL}/coins/markets?vs_currency=usd&per_page=10&page=1&x_cg_demo_api_key=${API_KEY}`;
+  return `${BASE_URL}/coins/markets?vs_currency=usd&per_page=25&page=1&x_cg_demo_api_key=${API_KEY}`;
 };
 
 export { getCoinnList };
