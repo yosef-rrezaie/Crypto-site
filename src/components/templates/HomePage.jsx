d1400fc55d4403a42347eb2cb4b5443b972c73e3
yosef-rrezaie <yosefrrezaie@gmail.com>
2024-12-18 14:12:42 +0330

    change currency by api

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -2,22 +2,25 @@ import React, { useEffect, useState } from "react";
 import TableCoin from "../modules/TableCoin";
 import { getCoinnList } from "../../services/CryptoApi";
 import Pagination from "../modules/Pagination";
+import Search from "../modules/Search";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("usd");
   useEffect(() => {
     setIsLoading(true);
-    fetch(getCoinnList(page))
+    fetch(getCoinnList(page, currency))
       .then((res) => res.json())
       .then((json) => setCoins(json));
 
     setIsLoading(false);
-  }, [page]);
+  }, [page, currency]);
   return (
     <div>
-      <TableCoin coins={coins} isLoading={isLoading} />
+      <Search currency={currency} setCurrency={setCurrency} />
+      <TableCoin coins={coins} isLoading={isLoading}/>
       <Pagination page={page} setPage={setPage} />
     </div>
   );
