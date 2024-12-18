0a304b8e42e2aba6db80921d65feb045769e6f51
Yosef Rezaie <yosefrezaie5001@gmail.com>
2024-12-18 13:41:38 +0330

    Pagination was also added by clicking

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -13,6 +13,10 @@ function Pagination({ page, setPage }) {
     if (page >= 10) return;
     setPage((page) => page + 1);
   };
+
+  const clickHandler = (e) => {
+    setPage(Number(e.target.innerText));
+  };
   return (
     <div className={styles.pagination}>
       <button
@@ -21,8 +25,12 @@ function Pagination({ page, setPage }) {
       >
         previous
       </button>
-      <p className={page === 1 ? styles.selected : null}>1</p>
-      <p className={page === 2 ? styles.selected : null}>2</p>
+      <p onClick={clickHandler} className={page === 1 ? styles.selected : null}>
+        1
+      </p>
+      <p onClick={clickHandler} className={page === 2 ? styles.selected : null}>
+        2
+      </p>
       {page > 2 && page < 9 && (
         <>
           <span>...</span>
@@ -30,8 +38,15 @@ function Pagination({ page, setPage }) {
         </>
       )}
       <span>...</span>
-      <p className={page === 9 ? styles.selected : null}>9</p>
-      <p className={page === 10 ? styles.selected : null}>10</p>
+      <p onClick={clickHandler} className={page === 9 ? styles.selected : null}>
+        9
+      </p>
+      <p
+        onClick={clickHandler}
+        className={page === 10 ? styles.selected : null}
+      >
+        10
+      </p>
       <button
         onClick={nextHandler}
         className={page === 10 ? styles.disabled : null}
