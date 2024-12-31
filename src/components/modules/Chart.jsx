c0e177f5c61da563c8e88b129f7b85d495b8316a
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-01-01 02:12:22 +0330

    Add graph conversion feature

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -18,50 +18,81 @@ function Chart({ chart, setChart }) {
   console.log(convertData(chart, type));
   console.log(chart);
 
+  function changeType(e) {
+    if (e.target.innerText === "Prices") {
+      setType(e.target.innerText.toLowerCase());
+    } else if (e.target.innerText === "Market Caps") {
+      setType("market_caps");
+    } else {
+      setType("total_volumes");
+    }
+  }
+
   return (
-    <div className={styles.container}>
-      <span className={styles.cross} onClick={() => setChart(null)}>
+    <div className={styles.container} onClick={() => setChart(null)}>
+      {/* <span className={styles.cross} onClick={() => setChart(null)}>
         X
-      </span>
+      </span> */}
       <div className={styles.chart}>
         <div className={styles.name}>
           <img src={chart.image} alt="" />
           <p>{chart.name}</p>
         </div>
-        <div className={styles.graph}>
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart width={400} height={400} data={convertData(chart, type)}>
-              <Line
-                type="monotone"
-                dataKey={type}
-                stroke="#3874ff"
-                strokeWidth="2px"
-              />
-              <CartesianGrid stroke="#404042" />
-              <YAxis dataKey={type} domain={["auto", "auto"]} />
-              <XAxis dataKey="date" hide />
-              <Legend />
-              <Tooltip />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
-        <div className={styles.types}>
-          <button>Prices</button>
-          <button>Market Caps</button>
-          <button>Total Volumes</button>
-        </div>
-        <div className={styles.details}>
-          <div>
-            <p>Prices:</p>
-            <span>${chart.current_price}</span>
+        <div onClick={(e) => e.stopPropagation()}>
+          <div className={styles.graph}>
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart
+                width={400}
+                height={400}
+                data={convertData(chart, type)}
+              >
+                <Line
+                  type="monotone"
+                  dataKey={type}
+                  stroke="#3874ff"
+                  strokeWidth="2px"
+                />
+                <CartesianGrid stroke="#404042" />
+                <YAxis dataKey={type} domain={["auto", "auto"]} />
+                <XAxis dataKey="date" hide />
+                <Legend />
+                <Tooltip />
+              </LineChart>
+            </ResponsiveContainer>
           </div>
-          <div>
-            <p>ATH:</p>
-            <span>${chart.ath}</span>
+          <div className={styles.types}>
+            <button
+              className={type === "prices" ? styles.selected : null}
+              onClick={changeType}
+            >
+              Prices
+            </button>
+            <button
+              className={type === "market_caps" ? styles.selected : null}
+              onClick={changeType}
+            >
+              Market Caps
+            </button>
+            <button
+              className={type === "total_volumes" ? styles.selected : null}
+              onClick={changeType}
+            >
+              Total Volumes
+            </button>
           </div>
-          <div>
-            <p>Market Cap :</p>
-            <span>${chart.market_cap}</span>
+          <div className={styles.details}>
+            <div>
+              <p>Prices:</p>
+              <span>${chart.current_price.toLocaleString()}</span>
+            </div>
+            <div>
+              <p>ATH:</p>
+              <span>${chart.ath.toLocaleString()}</span>
+            </div>
+            <div>
+              <p>Market Cap :</p>
+              <span>${chart.market_cap.toLocaleString()}</span>
+            </div>
           </div>
         </div>
       </div>
